c3254caa8f677052150f85694b270c7e31ef657b
Teo <teokakabadze7@gmail.com>
2026-02-07 15:06:15 -0600

    finished the submissions table

--- app/admin/submissions/page.tsx
+++ app/admin/submissions/page.tsx
@@ -7,14 +7,17 @@ import {
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible"
-import { ChevronDown } from "lucide-react"
+import { ChevronDown, Play } from "lucide-react"
 
 function TableRow({ children }: { children: React.ReactNode }) {
     return (
-    <div className="rounded-xl border bg-white">
+    <div className="border border-slate-400 hover:border-slate-900 transition rounded-xl">
         {children}
     </div>)
 }
+
+const rowLayout = "grid grid-cols-5 items-center justify-items-center gap-x-6 px-6 py-3";
+
 
 export default function Page() {
     // group submissions by netid - CHANGE LATER FOR OTHER GROUPS
@@ -28,39 +31,40 @@ export default function Page() {
     }, {});
     return (
         <div className="mx-auto max-w-7xl px-4 lg:px-6">
-            <div>
+            <div className="space-y-4">
                 { /* submissions header table */ }
-                <div className="grid grid-cols-5 gap-4 items-center text-center rounded-xl bg-[#222d65] text-white px-4 py-5 text-sm font-medium">
-                    <span>NetID / Form Title</span>
+                <div className={`${rowLayout} rounded-xl bg-[#222d65] text-[#e7f0ff] px-4 py-5 mb-4 text-sm font-medium`}>
+                    <span className="justify-self-start px-10">NetID / Form Title</span>
                     <span>Form ID</span>
                     <span>Created At</span>
                     <span>Updated At</span>
-                    <span>Status</span>
+                    <span className="justify-self-start px-12">Status</span>
                 </div>
 
                 { /* actual submissions */ }
                 {Object.entries(groupedByNetID).map(([netid, submissions]) => (
-                    <Collapsible key={netid} className="rounded-xl border border-blue-100">
-                        <CollapsibleTrigger className="group w-full px-6 py-3 bg-blue-50 rounded-xl hover:bg-blue-100">
-                            <div className="flex items-center gap-2 font-semibold text-[#222d65]">
-                                <ChevronDown className="h-4 w-4 group-data-[state=open]:rotate-180" />
+                    <Collapsible key={netid}>
+                        <CollapsibleTrigger className={`${rowLayout} w-full group bg-[#e7f0ff] rounded-xl hover:bg-blue-100`}>
+                            <div className="flex items-center justify-self-start px-5 gap-2 font-semibold text-[#222d65]">
+                                <Play className="h-3 w-3 fill-[#222d65] -rotate-90 group-data-[state=open]:rotate-90 transition-none"/>
                                 {netid}
                             </div>
                         </CollapsibleTrigger>
 
-                        <CollapsibleContent className="px-6 pb-4 space-y-2">
+                        <CollapsibleContent className="pt-2 space-y-2">
                             {(submissions as any[]).map((s, i) => (
                             <TableRow key={i}>
-                                <div className="grid grid-cols-5 items-center px-6 py-3 text-sm text-[#222d65]">
-                                    <span>Form 1 {/* use actual form number when available */}</span>
-                                    <span>{s.id}</span>
+                                <div className={`${rowLayout} text-sm text-[#222d65]`}>
+                                    <span className="justify-self-start px-10">Form 1 {/* use actual form number when available */}</span>
+                                    <span>{s.id} {/*add zeros in front?*/}</span>
                                     <span>{s.created_at}</span>
                                     <span>{s.updated_at}</span>
                                     <span
                                         className={
-                                            s.status === "Checked In"
-                                            ? "text-green-600 font-semibold"
-                                            : "text-purple-600 font-semibold"
+                                            "justify-self-start px-10" +
+                                            (s.status === "Checked In"
+                                            ? " text-green-600 font-semibold"
+                                            : " text-purple-600 font-semibold")
                                         }
                                     >
                                         {s.status}
