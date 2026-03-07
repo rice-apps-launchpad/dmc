c0a6b787ad1cefd85793bd5bbe31a5384d7f4b26
Gabriel Ong <gabrielong2004@gmail.com>
2026-03-07 15:04:06 -0600

    Limit Suspense to just the table content

--- app/admin/forms/page.tsx
+++ app/admin/forms/page.tsx
@@ -14,6 +14,33 @@ async function formsData() {
 async function PageContent() {
     const data = await formsData();
 
+    if (data === null)
+        return <p>No forms found.</p>
+
+    return (
+        data.map((form) => (
+            <TableRow key={form.id}>
+                <div className='flex justify-center items-center h-full'>
+                    <div className='mx-[40px] grid grid-cols-[1fr_2fr_250px] items-center gap-4 w-full'>
+                        <p className="whitespace-nowrap text-[14px] !font-inter !font-[400]">{form.title}</p>
+                        <p className='whitespace-nowrap text-[14px] !font-inter !font-[400]'>{form.description}</p>
+                        <div className='flex flex-row justify-between items-center gap-[15px]'>
+                            <Button variant='outline' className='!bg-[#E7F0FF] !border-[#222D65] !font-inter !font-[400] !text-[#222D65] !rounded-xl !px-[18px] !py-[5px]' asChild>
+                                <Link href={`forms/${form.id}`}>View</Link>
+                            </Button>
+                            <Button variant='outline' className='!bg-[#E7F0FF] !border-[#222D65] !font-inter !font-[400] !rounded-xl !px-[18px] !py-[5px]' asChild>
+                                <Link href={`forms/${form.id}/edit`}>Edit</Link>
+                            </Button>
+                            <Button variant='outline' className='!bg-white !border-[#B20000] !font-inter !font-[400] !text-[#B20000] !rounded-xl !px-[18px] !py-[5px]'>Delete</Button>
+                        </div>
+                    </div>
+                </div>
+            </TableRow>
+        ))
+    )
+}
+
+export default function Page() {
     return (
         <div className='flex flex-col gap-[25px]'>
             <div className="mt-[37px]">
@@ -24,34 +51,10 @@ async function PageContent() {
                     <span><strong>Form Title</strong></span>
                     <span><strong>Description</strong></span>
                 </div>
-                {(data !== null) ? data.map((form) => (
-                    <TableRow key={form.id}>
-                        <div className='flex justify-center items-center h-full'>
-                            <div className='mx-[40px] grid grid-cols-[1fr_2fr_250px] items-center gap-4 w-full'>
-                                <p className="whitespace-nowrap text-[14px] !font-inter !font-[400]">{form.title}</p>
-                                <p className='whitespace-nowrap text-[14px] !font-inter !font-[400]'>{form.description}</p>
-                                <div className='flex flex-row justify-between items-center gap-[15px]'>
-                                    <Button variant='outline' className='!bg-[#E7F0FF] !border-[#222D65] !font-inter !font-[400] !text-[#222D65] !rounded-xl !px-[18px] !py-[5px]' asChild>
-                                        <Link href={`forms/${form.id}`}>View</Link>
-                                    </Button>
-                                    <Button variant='outline' className='!bg-[#E7F0FF] !border-[#222D65] !font-inter !font-[400] !rounded-xl !px-[18px] !py-[5px]' asChild>
-                                        <Link href={`forms/${form.id}/edit`}>Edit</Link>
-                                    </Button>
-                                    <Button variant='outline' className='!bg-white !border-[#B20000] !font-inter !font-[400] !text-[#B20000] !rounded-xl !px-[18px] !py-[5px]'>Delete</Button>
-                                </div>
-                            </div>
-                        </div>
-                    </TableRow>
-                )) : <p>Data is null</p>}
+                <Suspense>
+                    <PageContent />
+                </Suspense>
             </div>
         </div>
-    );
-}
-
-export default function Page() {
-    return (
-        <Suspense>
-            <PageContent/>
-        </Suspense>
     );
 }
