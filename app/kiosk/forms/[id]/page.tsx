40fb4be0b15759f9d71c7cf27df73bf15ed4cdc5
Nathalie Rodriguez <119267283+nathaliezrz@users.noreply.github.com>
2026-04-18 15:19:38 -0500

    Kiosk/link form submission with supabase (#21)
    * Connected the form page to retrive images and item information from the Supabase.
    
    * Added useState for various props and onChange property to update data in FormInput
    
    ---------
    
    Co-authored-by: Leonard Gabriel Ong <45550118+leonriel@users.noreply.github.com>

--- app/kiosk/forms/[id]/page.tsx
+++ app/kiosk/forms/[id]/page.tsx
@@ -1,5 +1,21 @@
 'use client'
-import { Suspense, use } from 'react'
+import { Suspense, use, useEffect, useState } from 'react'
+import { createClient } from "@/lib/supabase/client";
+import {useRouter} from "next/navigation"
+import { Alert, AlertTitle } from '@/components/ui/alert'
+
+
+async function fetchFormData(id: number) {
+  const supabase = createClient();
+  const {data: formData} = await supabase.from("forms").select().eq("id", id).single()
+  return formData
+}
+
+async function getImageUrl(path: string) {
+  const supabase = createClient();
+  const { data } = await supabase.storage.from("equipment_images").getPublicUrl(path);
+  return data.publicUrl
+}
 
 import mockFormData from "@/lib/mock_form.json"
 import {
@@ -87,20 +103,26 @@ const styles = {
 }
 type EquipmentProps = {
     label: string[],
-    image: string[]
+    image: string[],
+    statuses: string[],
+    onStatusChange: (index: number, value: string) => void
 }
 
 type FormInputProps = {
     title: string,
     type: string,
-    placeholder: string
+    placeholder: string,
+    value: string,
+    onChange: (value: string) => void
 }
 
 const frameworks = ["Present", "Not Present"]
 
-function AvailabilityStatus() {
+type AvailabilityStatusProps = { value: string; onChange: (value: string) => void }
+
+function AvailabilityStatus({ value, onChange }: AvailabilityStatusProps) {
   return (
-    <Combobox items={frameworks}>
+    <Combobox items={frameworks} value={value} onValueChange={(val) => onChange(val ?? "")}>
       <ComboboxInput placeholder="Not Selected" style={styles.combobox}/>
       <ComboboxContent>
         <ComboboxEmpty>No items found.</ComboboxEmpty>
@@ -116,7 +138,30 @@ function AvailabilityStatus() {
   )
 }
 
-function EquipmentList({label, image}: EquipmentProps){
+const AlertIndicatorSuccessDemo = () => {
+  return (
+    <Alert className='rounded-md border-l-6 border-green-600 bg-green-600/10 text-green-600 dark:border-green-400 dark:bg-green-400/10 dark:text-green-400'>
+      <AlertTitle className="text-center">Form submitted successfully.</AlertTitle>
+    </Alert>
+  )
+}
+
+const AlertIndicatorMissingDemo = () => {
+  return (
+    <Alert className='rounded-md border-l-6 border-red-600 bg-red-600/10 text-red-600 dark:border-red-400 dark:bg-red-400/10 dark:text-red-400'>
+      <AlertTitle className="text-center">Please fill out all fields.</AlertTitle>
+    </Alert>
+  )
+}
+const AlertIndicatorInternalErrorDemo = () => {
+  return (
+    <Alert className='rounded-md border-l-6 border-red-600 bg-red-600/10 text-red-600 dark:border-red-400 dark:bg-red-400/10 dark:text-red-400'>
+      <AlertTitle className="text-center">Internal error occurred. Please try again.</AlertTitle>
+    </Alert>
+  )
+}
+
+function EquipmentList({label, image, statuses, onStatusChange}: EquipmentProps){
     return(
         <div className="flex flex-column align-center gap-[100px] justify-start items-center flex-wrap">
             {label.map((label, index) => (
@@ -125,7 +170,7 @@ function EquipmentList({label, image}: EquipmentProps){
                 <img className="h-[100%]" src={image[index]} alt={label} /> </div>
                 <p className="text-[24px] mb-[10px] mt-[5px]">{label}</p>
                 <div>
-                  {<AvailabilityStatus />}
+                  <AvailabilityStatus value={statuses[index]} onChange={(val) => onStatusChange(index, val)} />
                 </div>
             </div>
         ))}
@@ -136,17 +181,98 @@ function FormInput(props: FormInputProps) {
   return (
     <div className="flex flex-col">
       <p className="text-[24px]">{props.title}</p>
-      <input style={styles.input} type={props.type} placeholder= {props.placeholder}></input>
+      <input style={styles.input} type={props.type} placeholder={props.placeholder} value={props.value} onChange={(e) => props.onChange(e.target.value)}></input>
     </div>
   );
 }
 
+type TForm = {
+  title: string,
+  description: string,
+  equipment_labels: string[],
+  equipment_images: string[],
+  category: string
+}
+
+
 function SuspendedFormPage() {
   const { id } = useParams<{ id: string }>()
   const numericId = Number(id)
-  const form = mockFormData.find(item => item.id === numericId)
-  
- 
+  const [form, setForm] = useState<TForm>({title: "", description: "", equipment_labels: [], equipment_images: [], category: ""});
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const [netId, setNetId] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [dueTime, setDueTime] = useState("");
+  const [staffName, setStaffName] = useState("");
+  const [equipmentStatuses, setEquipmentStatuses] = useState<string[]>([]);
+  const [submitted, setSubmitted] = useState(false);
+  const [clicked, setClicked] = useState(false);
+  const [missingFields, setMissingFields] = useState(false);
+  const [internalError, setInternalError] = useState(false);
+
+  const router = useRouter();
+
+
+  useEffect(() => {
+    async function getFormData() {
+      
+      const data = await fetchFormData(numericId);
+      setForm(data);
+      setEquipmentStatuses(new Array(data.equipment_labels.length).fill(""));
+    }
+    getFormData();
+  }, [setForm, fetchFormData, numericId])
+
+  useEffect(() => {
+    async function fetchImageUrls() {
+      const urls = await Promise.all(
+        form?.equipment_images?.map(async (image: string) => await getImageUrl(image)) ?? []
+      );
+
+      console.log(urls);
+      setImageUrls(urls);
+    }
+    fetchImageUrls();
+  }, [form?.equipment_images, setImageUrls, getImageUrl]) 
+
+  async function handleSubmit(buttonElement: HTMLButtonElement) {
+  setClicked(true);
+  buttonElement.disabled = true;
+
+  if (!netId || !dueDate || !dueTime || !staffName || equipmentStatuses.includes("")) {
+    setMissingFields(true);
+    setInternalError(false);
+    setSubmitted(false);
+    buttonElement.disabled = false;
+    return;
+  }
+  setMissingFields(false);
+
+  const supabase = createClient();
+  const { error } = await supabase.from("submissions").insert({
+    netid: netId,
+    due_date: dueDate,
+    due_time: dueTime,
+    checkout_staff: staffName,
+    checkout_responses: equipmentStatuses.map(s => s === "Present"),
+    title: form.title,
+    equipment_labels: form.equipment_labels,
+    equipment_images: form.equipment_images,
+    category: form.category,
+    description: form.description,
+    status: "Checked Out",
+  });
+
+  if (error) {
+    console.log("Error submitting form:", error);
+    setInternalError(true);
+    setSubmitted(false);
+    buttonElement.disabled = false;
+
+  } else {
+    setSubmitted(true);
+    setTimeout(() => router.push("/kiosk"), 2000);
+  }}
   return (
     <div style={styles.page}>
         <div style={styles.formTitle}>
@@ -158,19 +284,45 @@ function SuspendedFormPage() {
         <div style={styles.middleSection}>
             <h1 className="font-bold text-[24px]">Equipment Details</h1>
             <div>
-                <EquipmentList image={form?.equipment_images ?? []} label={form?.equipment_labels ?? []}/>
+                <EquipmentList
+                  image={imageUrls}
+                  label={form?.equipment_labels ?? []}
+                  statuses={equipmentStatuses}
+                  onStatusChange={(index, val) => setEquipmentStatuses(prev => {
+                    const updated = [...prev];
+                    updated[index] = val;
+                    return updated;
+                  })}
+                />
             </div>
             <h1 className=" mt-[65px] font-bold text-[24px] mb-[15px]">Other</h1>
             <div style={styles.otherSection}>
-              <FormInput title={"NetId"} type={"text"} placeholder={'Add your NetId'}/>
-              <FormInput title={"Due Date"} type={"date"} placeholder={'Calendar Picker'}/>
-              <FormInput title={"Due Time"} type={"time"} placeholder={'Time Picker'}/>
-              <FormInput title={"DMC Staff Member's Name"} type={"text"} placeholder={"Add DMC Member's Name"}/>
+              <FormInput title={"NetId"} type={"text"} placeholder={'Add your NetId'} value={netId} onChange={setNetId}/>
+              <FormInput title={"Due Date"} type={"date"} placeholder={'Calendar Picker'} value={dueDate} onChange={setDueDate}/>
+              <FormInput title={"Due Time"} type={"time"} placeholder={'Time Picker'} value={dueTime} onChange={setDueTime}/>
+              <FormInput title={"DMC Staff Member's Name"} type={"text"} placeholder={"Add DMC Member's Name"} value={staffName} onChange={setStaffName}/>
             </div>
         </div>
         <hr className="h-[1px] w-full border-[0.5px] border-[#9f9f9f]"></hr>
+        {clicked &&submitted && (
+          <div className="w-[40%] mb-4, mt-4">
+            <AlertIndicatorSuccessDemo />
+          </div>
+        )}
+        {missingFields && (
+          <div className="w-[40%] mb-4, mt-4">
+            <AlertIndicatorMissingDemo />
+          </div>
+        )}
+        {internalError && clicked && !submitted && (
+          <div className="w-[40%] mb-4, mt-4">
+            <AlertIndicatorInternalErrorDemo />
+          </div>
+        )}
         <div style={styles.bottomSection}>
-            <button style={styles.button}>Submit</button>     
+            <button style={styles.button} onClick={(e) => handleSubmit(e.currentTarget)}>
+                Submit
+            </button>
         </div>
     </div>
   )
